b86c7023573c3b1756b1b10524a0c402444a5764
Verit Studio <41865259+veritstudio@users.noreply.github.com>
2018-07-29 21:34:22 +0200

    Create hello.js

---
+++ src/js/hello.js
@@ -0,0 +1,16 @@
+class Hello extends React.Component {
+ 
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+      </div>
+    );
+  }
+}
+  
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('root')
+);
+  
